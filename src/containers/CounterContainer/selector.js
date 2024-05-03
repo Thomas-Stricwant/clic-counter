338c2e668ebb5ac2e48d5f0650e3801001d1e19d
Thomas-Stricwant <tomstricky@live.be>
2024-05-03 08:29:20 +0200

    feat : removed an import that could be exported in 1 line

--- src/containers/CounterContainer/selector.js
+++ src/containers/CounterContainer/selector.js
@@ -2,8 +2,4 @@ import { createSelector } from 'reselect';
 import { initialState } from './reducer';
 
 const selectHomeContainerDomain = state => state.counterContainer || initialState;
-const makeSelectHomeContainerCounter = () => createSelector(selectHomeContainerDomain, substate => substate.counter);
-
-export {
-  makeSelectHomeContainerCounter,
-};
+export const makeSelectHomeContainerCounter = () => createSelector(selectHomeContainerDomain, substate => substate.counter);
